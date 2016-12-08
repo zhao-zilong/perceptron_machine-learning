20a2fd27b627e45c41703a4f24c2b2307fcc58f3
赵子龙 <zilong_zhao@foxmail.com>
2016-12-08 09:34:23 +0100

    correct a logic error in perceptron.sh

--- perceptron.sh
+++ perceptron.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+    ./pre-traiter.pl spambase/spambase.data
 for i in {1..20}
-  do
-     ./pre-traiter.pl spambase/spambase.data
+  do     
      ./repartir_aleatoire.pl format
      ./perceptron.pl apprentissage
      ./verifier.pl test hyperplan
